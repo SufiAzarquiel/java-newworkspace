01ba548a2ed2def724f001efd9ed22fa51d93a56
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2023-03-06 10:06:23 +0100

    Move packages to other repo

--- src/main/java/com/github/sufiazarquiel/workspace/Restaurante/Pescado.java
+++
@@ -1,8 +0,0 @@
-package com.github.sufiazarquiel.workspace.restaurante;
-
-public class Pescado extends Plato {
-    // Constructor
-    public Pescado(String nombre, double precio) {
-        super(nombre, precio);
-    }
-}
