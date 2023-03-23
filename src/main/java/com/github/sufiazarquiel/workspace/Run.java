e85149accf46d797a22124b8d436f89e5ec12f4a
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2023-03-23 10:13:43 +0100

    PROG 23/03 2

--- src/main/java/com/github/sufiazarquiel/workspace/Run.java
+++ src/main/java/com/github/sufiazarquiel/workspace/Run.java
@@ -1,32 +1,13 @@
 package com.github.sufiazarquiel.workspace;
 
-import java.util.ArrayList;
-
 public class Run {
     // Main method
     public static void main(String[] args) {
        System.out.println("Hi");
     }
 
-    // Attributes
-    private int a, b;
-    private ArrayList<Integer> arraylist;
-    private int[] array;
+    private int a;
 
-    // Constructors
-    public Run() {
-        this.a = 0;
-        this.b = 0;
-        this.arraylist = new ArrayList<Integer>();
-        this.array = new int[8];
-    }
-
-    public Run(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    // Getters and Setters
     public int getA() {
         return a;
     }
@@ -35,55 +16,11 @@ public class Run {
         this.a = a;
     }
 
-    public int getB() {
-        return b;
+    public Run(int a) {
+        this.a = a;
     }
 
-    public void setB(int b) {
-        this.b = b;
-    }
-
-    
-    public ArrayList<Integer> getArraylist() {
-        return arraylist;
-    }
-    
-    public void setArraylist(ArrayList<Integer> arraylist) {
-        this.arraylist = arraylist;
-    }
-
-    public int[] getArray() {
-        return array;
-    }
-
-    public void setArray(int[] array) {
-        this.array = array;
-    }
-
-    public int getIntAtArrayList(int index) {
-        return this.arraylist.get(index);
-    }
-
-    public int getIntAtArray(int index) {
-        return this.array[index];
-    }
-    
-    // Methods
-    public void addToArrayList(int a) {
-        this.arraylist.add(a);
-    }
-
-    public void addToArray(int a, int index) {
-        this.array[index] = a;
-    }
-
-    public void removeFromArrayList(int a) {
-        this.arraylist.remove(a);
-    }
-
-    // ToString
-    @Override
-    public String toString() {
-        return "Run object [a=" + a + ", b=" + b + "]";
+    public Run() {
+        this.a = 0;
     }
 }
--- src/test/java/com/github/sufiazarquiel/workspace/RunTest.java
+++ src/test/java/com/github/sufiazarquiel/workspace/RunTest.java
@@ -3,8 +3,6 @@ package com.github.sufiazarquiel.workspace;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import com.github.sufiazarquiel.workspace.oneFilers.OOP1Homework;
-
 /**
  * Unit-level testing for {@link Run} object.
  */
@@ -12,8 +10,7 @@ public class RunTest {
     @Test
     public void classTest() {
         Run oRun = new Run();
-        assertEquals(0, oRun.getA());
-        assertEquals(12.153, OOP1Homework.redondear(12.153123, 3));
-        assertEquals(123.152, OOP1Homework.redondear(123.152, 4));
+        oRun.setA(1);
+        assertEquals(oRun.getA(), 1);
     }
 }
